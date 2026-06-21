dbb632ea5e435f5b4f9e114b74db663f9c8dc3e6
Sanskar Singh <work.sanskarr@gmail.com>
2026-06-21 23:30:13 +0530

    prepare backend for deployment

--- server.js
+++ server.js
@@ -61,9 +61,7 @@ app.post("/api/query", async (req, res) => {
   }
 });
 
-const PORT = 3001;
-if (!process.env.VERCEL) {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
